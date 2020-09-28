3640ecbc3826624210f5292fd634de27aef3ea8d
Caio André <caio_andre15@hotmail.com>
2020-09-28 12:20:36 -0300

    Adicionado exer02 - Recuperar Dados

---
+++ src/courseJDBC/exer02/db/DbException.java
@@ -0,0 +1,11 @@
+package courseJDBC.exer02.db;
+
+public class DbException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DbException(String msg) {
+		super(msg);
+	}
+
+}
