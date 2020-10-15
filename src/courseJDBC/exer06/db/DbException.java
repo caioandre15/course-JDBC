51dbe8c07a7fd38ba16d6c660850e055341a3e74
Caio André <caio_andre15@hotmail.com>
2020-10-14 22:03:13 -0300

    Adicionado Exercicio 06

---
+++ src/courseJDBC/exer06/db/DbException.java
@@ -0,0 +1,11 @@
+package courseJDBC.exer06.db;
+
+public class DbException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DbException(String msg) {
+		super(msg);
+	}
+
+}
